f7fbfd83865c470161a383188b49444e5413a8e6
buxiangdong <819453411@qq.com>
2024-03-08 22:12:56 +0800

    test

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mainReducer from '../pages/main/mainSlice';
 
+// test
 export const store = configureStore({
   reducer: {
     main: mainReducer,
